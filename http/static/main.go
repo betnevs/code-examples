2ced0f6cfedbefb406cd206163fb440042904b1e
betNevS <1173325467@qq.com>
2022-06-20 15:36:34 +0800

    add mapstructure examples

---
+++ http/static/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.ListenAndServe(":8081", nil)
+}
